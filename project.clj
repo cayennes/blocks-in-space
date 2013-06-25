484eff8cf54889944ebe9ca3edbab11385615793
Cayenne <cayennes@gmail.com>
2013-06-25 11:50:46 -0500

    Initial commit
    .gitignore and project.clj

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject blocks-in-space "0.1.0-SNAPSHOT"
+  :description "A falling block game in 3D space"
+  :url "https://github.com/cayennes/blocks-in-space"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [quil "1.6.0"]]
+  :main blocks-in-space.core)
