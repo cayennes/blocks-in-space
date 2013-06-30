cba84069a3ec40adc81f5361f8724da7797c867f
Cayenne <cayennes@gmail.com>
2013-06-30 18:06:58 -0500

    Define blocks via strings
    Containing visual representation

--- src/blocks_in_space/core.clj
+++ src/blocks_in_space/core.clj
@@ -26,21 +26,53 @@
 
 ;; Block manipulation
 
-(def starting-shapes
-  [#{[0 0 0] [1 0 0] [2 0 0] [-1 0 0]}
-   #{[0 0 0] [1 0 0] [1 1 0] [-1 0 0]}
-   #{[0 0 0] [1 0 0] [0 1 0] [1 1 0]}
-   #{[0 0 0] [-1 0 0] [1 0 0] [0 1 0]}
-   #{[0 0 0] [-1 0 0] [0 1 0] [1 1 0]}
-   #{[0 0 0] [-1 0 0] [0 0 -1] [0 1 -1]}
-   #{[0 0 0] [-1 0 0] [0 0 -1] [0 -1 -1]}
-   #{[0 0 0] [1 0 0] [0 1 0] [0 0 1]}])
+(defn parse-block-string
+  "Take a string containing a human readable representation of a 3-dimensional
+   block and return a vector of coordinates of cubes"
+  [block-str]
+  (->> block-str
+       (#(clojure.string/split % #"\n")) ; split the lines
+       (map #(clojure.string/split % #" +")) ; split the parts
+       (map (partial filter #(pos? (count %)))) ; drop remnants of initial whitespace
+       (map #(map seq %)) ; convert strings to lists of chars
+       ; now the outermost list represents y,
+       ;     the sublists z,
+       ;     and sublists of that represent x
+       (map-indexed (fn [y l] (map (partial map #(vector [y] %)) l)))
+       (map #(map-indexed (fn [z l] (map (fn [[[y] s]] [[y z] s]) l)) %))
+       (map (partial map #(map-indexed (fn [x [[y z] c]] [[x y z] c]) %)))
+       (apply concat) (apply concat)
+       ; now we have tuples of coordinate and character
+       ((fn [l]
+          (let [center (first (first (drop-while (fn [[v c]] (and (not= \, c) (not= \X c))) l)))]
+          (map (fn [[v c]] [(map #(- %1 %2) v center) c]) l))))
+       ; now it's shifted so that the X or , is at the center
+       (filter (fn [[v c]] (or (= c \x) (= c \X))))
+       (map first)))
 
-(def easier-shapes
-  [#{[0 0 0]}
-   #{[0 0 0] [1 0 0]}
-   #{[0 0 0] [1 0 0] [-1 0 0]}
-   #{[0 0 0] [1 0 0] [0 1 0]}])
+(def starting-shapes
+  (map parse-block-string ["xXxx"
+
+                           "..x
+                            xXx"
+
+                           ".x.
+                            xXx"
+
+                           "xx
+                            Xx"
+
+                           ".xx
+                            xX."
+
+                           "Xx x.
+                            x. .."
+
+                           ".. x.
+                            Xx x."
+
+                           ".. .x
+                            xX .x"]))
 
 (def blocks
   (map (fn [s] {:center [0 0 0] :shape s}) starting-shapes))
