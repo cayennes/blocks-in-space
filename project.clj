cd349d449bfb5e11723343127f5952911cc91d66
Cayenne Geis <cayennes@gmail.com>
2014-07-31 19:08:32 -0400

    Update clojure and quil
    This made the difference between not running on my new computer to
    working perfectly while spewing NPEs in the console

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/cayennes/blocks-in-space"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [quil "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [quil "2.2.0"]
                  [overtone/at-at "1.2.0"]]
   :main blocks-in-space.core)
